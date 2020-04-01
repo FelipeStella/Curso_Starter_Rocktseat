d73d8eb63b3123881282b3187a7761404e7c162f
FelipeStella <felipedasilva.stella@gmail.com>
2020-04-01 19:08:56 -0300

    Exercício 1 do módulo 2

---
+++ modulo_2/exercicio#1/main.js
@@ -0,0 +1,15 @@
+//1.1
+import ClasseUsuario from '../modulo_2/exercicio#1/functions';
+
+ClasseUsuario.info();
+
+//1.2
+import { idade } from '../modulo_2/exercicio#1/functions';
+
+console.log(idade);
+
+//1.3
+import ClassUser, { idade as IdadeUsuario } from '../modulo_2/exercicio#1/functions';
+
+ClassUser.info();
+console.log(IdadeUsuario);
